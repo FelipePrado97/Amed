09431ce9203c344ed12a4f3aa27f30c7713c9498
FelipePrado97 <felipe_prado97@hotmail.com>
2022-09-19 12:55:44 -0400

    19-09

---
+++ database/migrations/2022_09_19_144338_create_forms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('titulo',100);
+            $table->string('tipo', 100);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+};
